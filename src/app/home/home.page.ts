9f41de278feeedf800f3ca8bb389df4504485e49
WaldronSoftware <ronanwaldronsoftware@gmail.com>
2022-05-06 14:37:18 +0100

    Finished Weather Display. Added Landing Page

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -13,25 +13,28 @@ export class HomePage {
 
   weatherTemp :any
   todayDate = new Date()
-  cityName :any
+  cityName = ""
   weatherIcon :any 
   weatherDetails :any
+  name=""
+  loading = true
   //topIcon :any 
   
 
   constructor(public httpClient:HttpClient) {
-    this.loadData();
+    //this.loadData();
   }
 
     loadData(){
-      this.httpClient.get(`${API_URL}/weather?q=${"Dublin"}&appid=${API_KEY}`).subscribe(results =>{
+      this.httpClient.get(`${API_URL}/weather?q=${this.cityName}&appid=${API_KEY}`).subscribe(results =>{
         console.log(results);
         this.weatherTemp = results['main']
-        this.cityName = results['name']
+        this.name = results['name']
         console.log(this.weatherTemp);
         this.weatherDetails = results['weather'][0]
         console.log(this.weatherDetails);
         this.weatherIcon = `http://openweathermap.org/img/wn/${this.weatherDetails.icon}@2x.png`
+        this.loading = false
         //this.topIcon = `http://openweathermap.org/img/wn/10d@2x.png`
         
       
