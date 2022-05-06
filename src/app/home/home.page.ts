67436cedfdf06c0dcfdc9712d18f3bfa5f225574
WaldronSoftware <ronanwaldronsoftware@gmail.com>
2022-05-06 11:24:37 +0100

    Displaying Date and Cityname properly

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -11,15 +11,23 @@ const API_KEY = environment.API_KEY;
 })
 export class HomePage {
 
+  weatherTemp :any
+  todayDate = new Date()
+  cityName :any
   constructor(public httpClient:HttpClient) {
     this.loadData();
   }
 
     loadData(){
-      this.httpClient.get(`${API_URL}/weather?q=${"Dublin"}&appid=${API_KEY}`).subscribe(results =>{console.log(results);})
+      this.httpClient.get(`${API_URL}/weather?q=${"Dublin"}&appid=${API_KEY}`).subscribe(results =>{
+        console.log(results);
+        this.weatherTemp = results['main']
+        this.cityName = results['name']
+        console.log(this.weatherTemp);
+        
       
+      })
     }
-
   }
 
 
